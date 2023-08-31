445e16f20346bb8366b75d06f255c82e1320bdd6
karanchaure <karankchaure@gmail.com>
2023-08-31 09:25:00 +0000

    added security in user-microservice and created user feignclient interceptor for putting the token into the header when making request to other microservice

--- userservice/src/main/java/com/lcwd/user/service/userservice/config/MyConfig.java
+++ userservice/src/main/java/com/lcwd/user/service/userservice/config/MyConfig.java
@@ -3,13 +3,31 @@ package com.lcwd.user.service.userservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MyConfig {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    // declare the bean of OAuth2AuthorizedClientManager manager
+    public OAuth2AuthorizedClientManager manager(ClientRegistrationRepository repository,
+            OAuth2AuthorizedClientRepository clientRepository) {
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                repository, clientRepository);
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials()
+                .build();
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+        return defaultOAuth2AuthorizedClientManager;
+    }
+
 }
