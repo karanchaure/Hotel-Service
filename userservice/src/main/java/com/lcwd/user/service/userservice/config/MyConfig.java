432f0fb2e6644020a9b74511f5edc41871591154
karanchaure <karankchaure@gmail.com>
2023-08-31 10:04:35 +0000

    implemented interceptor for rest template

--- userservice/src/main/java/com/lcwd/user/service/userservice/config/MyConfig.java
+++ userservice/src/main/java/com/lcwd/user/service/userservice/config/MyConfig.java
@@ -1,8 +1,13 @@
 package com.lcwd.user.service.userservice.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
@@ -11,15 +16,28 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedCli
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
+import com.lcwd.user.service.userservice.config.interceptor.RestTemplateInterceptor;
+
 @Configuration
 public class MyConfig {
+
+    @Autowired
+    public ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository;
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new RestTemplateInterceptor(manager(clientRegistrationRepository, auth2AuthorizedClientRepository)));
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 
     // declare the bean of OAuth2AuthorizedClientManager manager
+    @Bean
     public OAuth2AuthorizedClientManager manager(ClientRegistrationRepository repository,
             OAuth2AuthorizedClientRepository clientRepository) {
         DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
